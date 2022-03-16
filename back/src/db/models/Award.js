09b5e34912b79896010ae73539fe531c148ba16e
JunSeokCheon <goosw775@naver.com>
2022-03-16 12:41:51 +0900

    DB Award 모델 구현

---
+++ back/src/db/models/Award.js
@@ -0,0 +1,17 @@
+/*
+  awardService에서 매개변수로 넘어온 입력값에 맞게 DB(AwardModel)에 새로운 데이터를 저장하거나, 찾거나, 갱신하거나, 삭제하여 awardService로 return
+  천준석
+  2022/03/16
+*/
+import {AwardModel} from "../schemas/award";
+
+class Award {
+  // 새로운 수상이력에 대한 정보를 DB에 만들고 return
+  static async create({newAward}) {
+    const createdNewAward = await AwardModel.create(newAward);
+    return createdNewAward;
+  }
+
+}
+
+export {Award}
