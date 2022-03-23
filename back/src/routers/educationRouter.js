361e32d6ba426da9ae1b6ddee277828ebc06d436
brilence75 <brilence75@gmail.com>
2022-03-23 21:50:24 +0900

    (백) education-mvp 비공개 설정 완료

--- back/src/routers/educationRouter.js
+++ back/src/routers/educationRouter.js
@@ -3,7 +3,15 @@
   
   2022/03/17
   김보현
-*/
+
+ * <education 비공개 설정 구현>
+ * 작성자 : 장정민, 일자 : 2022-03-23
+ * - educationRouter.post('/education/create') : req.body에서 isPrivate 필드도 받아와서 리턴
+ * - educationRouter.put('/educations/:id') : isPrivate 필드도 업데이트 가능하도록 수정
+ * - educationRouter.get('/educationlist/:user_id') : 파라미터에 currentUserId를 추가해서 1)본인 페이지 접근 2)다른 유저의 페이지 접근 시 db에서 반환하는 데이터를 구분한다.
+ * 
+ *
+ */
 
 import is from "@sindresorhus/is";
 import { Router } from "express";
@@ -22,16 +30,14 @@ educationRouter.post("/education/create", async function (req, res, next) {
       );
     }
 
-    const {user_id} = req.body;
-    const {school} = req.body;
-    const {major} = req.body;
-    const {position} = req.body;
+    const {user_id, school, major, position, isPrivate} = req.body;
 
     const newEducation = await EducationService.addEducation({
       user_id,
       school,
       major,
-      position
+      position,
+      isPrivate
     });
 
     res.status(201).json(newEducation);
@@ -59,8 +65,10 @@ educationRouter.get("/educations/:id", async function(req, res, next){
 // 특정 user_id를 이용해 학력 정보들을 찾기 위한 get요청
 educationRouter.get("/educationlist/:user_id", async function (req, res, next) {
   try {
+    //req에서 currentUserId 가져오기, login_required 파일 참고
+    const currentUserId =req.currentUserId;
     const {user_id} = req.params;
-    const educationList = await EducationService.getEducationList({ user_id });
+    const educationList = await EducationService.getEducationList({ currentUserId, user_id });
     res.status(200).send(educationList);
   } catch (error) {
     next(error);
@@ -71,16 +79,17 @@ educationRouter.get("/educationlist/:user_id", async function (req, res, next) {
 educationRouter.put('/educations/:id', async function (req, res, next){
   try {
     const educationId = req.params.id;
-
     
     const school = req.body.school ?? null;
     const major = req.body.major ?? null;
     const position = req.body.position ?? null;
+    const isPrivate = req.body.isPrivate ?? null;
 
     const toUpdate = { 
       school,
       major,
-      position
+      position,
+      isPrivate
     };
     
     const education = await EducationService.setEducation({ educationId, toUpdate });
