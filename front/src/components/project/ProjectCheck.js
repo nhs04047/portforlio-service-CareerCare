07c53341b61f961d1787a58e21c8ff9c15d53361
ys <lhyahoo0119@gmail.com>
2022-03-23 17:21:32 +0900

    비공개가 true일 때 비공개로 변경

--- front/src/components/project/ProjectCheck.js
+++ front/src/components/project/ProjectCheck.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-
 import { Form } from 'react-bootstrap';
+
+/**
+ * 비공개 기능 구현하는 component
+ * isPrivate가 false일 때 공개 true일 때 비공개
+ */
 
 function ProjectCheck({ isPrivate, setIsPrivate }) {
   //isPrivate, setIsPrivate는 string
   // isChecked는 boolean값이여야 하고 둘이 연결되야 함
-  //isPrivate가 false일 때 공개 true일 때 비공개
 
   const [ischecked, setIsChecked] = useState(false);
   function makeBoolean(str) {
@@ -29,7 +32,7 @@ function ProjectCheck({ isPrivate, setIsPrivate }) {
       <Form.Check
         type='switch'
         id='custom-switch'
-        label='공개'
+        label='비공개'
         onChange={handleChange}
         checked={makeBoolean(isPrivate)}
       />
