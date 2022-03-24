e02d61440601d51bbef4b2a842217369e92c3d15
JunSeokCheon <goosw775@naver.com>
2022-03-25 02:51:17 +0900

    window.location.protocol 배포를 위한 수정

--- front/src/api.js
+++ front/src/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const backendPortNumber = "5001";
 const serverUrl =
-  "http://" + window.location.hostname + ":" + backendPortNumber + "/";
-
+  window.location.protocol + "//" + window.location.hostname + ":" + backendPortNumber + "/";
 async function get(endpoint, params = "") {
   console.log(
     `%cGET 요청 ${serverUrl + endpoint + "/" + params}`,
