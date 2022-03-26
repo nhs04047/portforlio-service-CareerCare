f6bf47a33c2ad110d262572b4dd0e3c70096cda8
보현 김 <nhs04047@gmail.com>
2022-03-26 14:49:44 +0900

    models 주석 수정

--- back/src/db/models/Like.js
+++ back/src/db/models/Like.js
@@ -1,14 +1,19 @@
 /*
-  두 유저가 좋아요 관계인지 아닌지 알기 위한 함수
-  좋아요라면 -> 좋아요 객체 반환, 아니라면 -> null 반환
-  천준석
-  2022/03/25
+* 좋아요 기능 모델
+*
+* <like 스키마 데이터 조작 모델 구현>
+* 작성자 : 최준석
+* 작성일 : 2022.03.25
+* 두 유저가 좋아요 관계인지 아닌지 알기 위한 함수
 */
 
 import { LikeModel } from "../schemas/like";
 
 class Like {
-  // 좋아요 버튼을 클릭한 user와 좋아요를 받은 user로 like 객체 생성
+  /*
+  * create()
+  * 좋아요 버튼을 클릭한 user와 좋아요를 받은 user로 like 객체 생성
+  */
   static async create({ currentUser, otherUser }) {
     const createdNewLike = await LikeModel.create({
       currentUser,
@@ -17,14 +22,21 @@ class Like {
     return createdNewLike;
   }
 
-  // 좋아요 버튼을 클릭한 user와 좋아요를 받은 user의 like 객체가 LikeModel안에 있다면 like 객체 반환/ 아니면 null 반환
+  /*
+  * findByUser()
+  * 좋아요 버튼을 클릭한 user와 좋아요를 받은 user의 like 객체가 LikeModel안에 있다면 like 객체 반환/ 아니면 null 반환
+  */
   static async findByUser({ currentUser, otherUser }) {
     const like = await LikeModel.findOne({
       $and: [{ currentUser }, { otherUser }],
     });
     return like;
   }
-  // 좋아요 버튼을 클릭한 user와 좋아요를 받은 user의 like 객체 삭제
+
+  /*
+  * deleteById
+  * 좋아요 버튼을 클릭한 user와 좋아요를 받은 user의 like 객체 삭제
+  */
   static async deleteById({ isLiked }) {
     const deleteResult = await LikeModel.deleteOne({
       _id : isLiked._id,
